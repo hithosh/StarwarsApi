33f74ed2b852460a1f9d9d66b687934df761f13b
hitosh <kotahithosh@gmail.com>
2022-04-18 14:41:08 +0100

    final code commit

--- src/Pages/People.tsx
+++ src/Pages/People.tsx
@@ -1,53 +1,106 @@
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import "bootstrap/dist/css/bootstrap.min.css"
+import Avatars from "../components/Avaatar";
 
+// declaring required props
 type resultProps = {
     name: string, 
-    height: number, 
-    mass: string, 
     hair_color:string, 
     skin_color: string, 
-    eye_color: string, 
-    birth_year: string, 
-    gender: string, 
-    
-};
+    next:string,
+    previous:string,
+    gender:string
+  };
 
- const StarApi=()=> {
-   
-  const [result, setResult] = useState<resultProps[]>([]);
-  const [search, setSearch] = useState("")
-  //calling Api using React hooks and handle promise object
+// create Functional component  for Pople
+const People=()=>{
+//decalare react hooks useState  for result ,pagination.searchdata
+const[result,setResult] = useState<resultProps[]>([]);
+const[previouss,setPrevious]=useState("");
+const[nexts,setNext]=useState("");
+const[search, setSearch] = useState("")
+
+//callback function for API calling
+const fectchApi = async (url:string) => {
+const data = await fetch(url, {
+      method: "GET"
+    });
+    const jsonData = await data.json();
+    setPrevious(jsonData.previous);
+    setNext(jsonData.next);
+    setResult(jsonData.results);
+  };
+  
+  //calling Api using React hooks  using useEffect
   useEffect(() => {
-    const fectchApi = async () => {
-      const data = await fetch(`https://swapi.dev/api/people/?search=${search}`, {
-        method: "GET"
-      });
-      const jsonData = await data.json();
-      setResult(jsonData.results);
-    };
-
-    fectchApi();
+    fectchApi(`https://swapi.dev/api/people/?search=${search}`);
   }, [search]);
 
-  return (
-    <div className="App">
-      <Header/>
-      <input type="text" value={search} onChange={(event)=>{setSearch(event.target.value)}}/>
-      <hr style={{height:"5px",backgroundColor:"black" }}/>
-      <div className="container" >
-        <div className="row">
-          { result.map((record, i)=> (
-            <div style={{ padding:"10px",backgroundColor:"orange",margin:"10px"}}className="col-lg-2 col-md-3 col-sm-3 col-12 mb-4 position-relative text-dark">
-              <span >Name: {record.name}</span><br/>
-              <span >Hair Color: {record.hair_color}</span><br/>
-               <span >Gender: {record.gender}</span>
-             </div>) ) }
-        </div>
+
+//next pagination  function
+function previous(e:any){
+  fectchApi(previouss)
+  }
+
+//previous  paginationa function
+function next(e:any){
+fectchApi(nexts)
+ }
+
+return (
+  <div className="App">
+    <Header />
+    <label>
+      Search{" "}
+      <input
+        type="text"
+        value={search}
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+    </label>
+    <hr style={{ height: "5px", backgroundColor: "black" }} />
+    <div className="container">
+      <div className="row">
+        {result.map((record, i) => (
+          <>
+            <div
+              style={{
+                padding: "10px",
+                backgroundColor: "#DBF9FC",
+                margin: "10px",
+                width: "23%",
+              }}
+              className="col-lg-2 col-md-3 col-sm-3 col-12 mb-4 position-relative text-dark"
+            >
+              <Avatars
+                avatharstyle={"Circle"}
+                hairColor={record.hair_color}
+                skincolor={record.skin_color}
+                gender={record.gender}
+              />
+              <div>
+                <span>Name: {record.name}</span>
+                <br />
+                <span>Hair Color: {record.hair_color}</span>
+                <br />
+                <span>Gender: {record.gender}</span>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
+      <button onClick={previous}>Previous</button>
+      <button style={{ margin: "10px" }} onClick={next}>
+        Next
+      </button>
     </div>
-    );
+  </div>
+);
 }
 
-export default StarApi;
+export default People;
+
+
